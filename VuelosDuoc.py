89a55dbc63e319c1a4c3faacd59d34977da7473a
Hernan Vallejos <hernanvallejos94@gmail.com>
2022-06-04 23:47:28 -0400

    subiendo archivo principal

---
+++ VuelosDuoc.py
@@ -0,0 +1,17 @@
+print("\t\tMENU AEREOLINEAS DUOC")
+print("*"*50)
+print("1.-Ver asientos disponibles\n2.-Comprar asientos\n3.-Anular vuelo\n4.-Modificar datos de pasajeros\n5.-Salir")
+print("*"*50)
+opcionM = int(input("\t\tIngrese una opcion: "))
+while opcionM != 4:
+    if opcionM == 1:
+        print("")
+
+    if opcionM == 2:
+        print("")
+
+    if opcionM == 3:
+        print("")
+
+    if opcionM == 4:
+        print("")
