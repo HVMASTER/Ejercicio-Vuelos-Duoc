72c6f32201a2344225b5280b61816da1b15126f1
HVMASTER <he.vallejos@duocuc.cl>
2022-06-05 15:01:37 -0400

    agregando datos a opcion 1 y opcion 2

--- VuelosDuoc.py
+++ VuelosDuoc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random as rd
+flag = True
 
 loteA = [[1,2,3],
          [7,8,9],
@@ -28,50 +29,55 @@ loteD = [[34,35,36],
 ladoD = np.array(loteD)
 
 
-
-print("\t\tMENU AEREOLINEAS DUOC")
-print("*"*50)
-print("1.-Ver asientos disponibles\n2.-Comprar asientos\n3.-Anular vuelo\n4.-Modificar datos de pasajeros\n5.-Salir")
-print("*"*50)
-opcionM = int(input("\t\tIngrese una opcion: "))
-print("\n")
-print("\t\tPRECIOS\nAsiento normal:\t\t$78.900\nAsiento VIP:\t\t$240.000")
-while opcionM != 4:
-    print("\n")
-    print("\t\t\tAsientos")
-    print("-"*52)
-    print("| 1\t2\t3\t\t4\t5\t6  |\n| 7\t8\t9\t\t10\t11\t12 |\n| 13\t14\t15\t\t16\t17\t18 |\n| 19\t20\t21\t\t22\t23\t24 |\n| 25\t26\t27\t\t28\t29\t30 |")
-    print("-"*19, "\t\t", "-"*19)
-    print("| 31\t32\t33\t\t34\t35\t36 |\n| 37\t38\t39\t\t40\t41\t42 |")
-    print("-"*52)
-    
-
-    if opcionM == 1:
-        numAsiento = int(input("Ingrese el numero de asiento que desea reservar: "))
+while flag:
+  print("\t\tMENU AEREOLINEAS DUOC")
+  print("*"*50)
+  print("1.-Ver asientos disponibles\n2.-Comprar asientos\n3.-Anular vuelo\n4.-Modificar datos de pasajeros\n5.-Salir")
+  print("*"*50)
+  opcionM = int(input("\t\tIngrese una opcion: "))
+  print("\n")
+  print("\t\tPRECIOS\nAsiento normal:\t\t$78.900\nAsiento VIP:\t\t$240.000")
 
 
-        if numAsiento >= 31 and numAsiento <= 42:
-            print("Usted ha escogido el tipo de asiento VIP")
-            cantAsientoV = int(input("Ingrese cuantos asientos VIP reservara: "))
-            if cantAsientoV > 0:
-                print("Usted ha reservado", cantAsientoV, "asientos VIP")
-
+  if opcionM == 1:
+        print("\n")
+        print("\t\t\tAsientos")
+        print("-"*52)
+        print("| 1\t2\t3\t\t4\t5\t6  |\n| 7\t8\t9\t\t10\t11\t12 |\n| 13\t14\t15\t\t16\t17\t18 |\n| 19\t20\t21\t\t22\t23\t24 |\n| 25\t26\t27\t\t28\t29\t30 |")
+        print("-"*19, "\t\t", "-"*19)
+        print("| 31\t32\t33\t\t34\t35\t36 |\n| 37\t38\t39\t\t40\t41\t42 |")
+        print("-"*52)
+        print("\t\tAsientos disponibles")
+        print("TIPO NORMAL:")
+        print(ladoA)
+        print("-"*10)
+        print(ladoB)
+        print("-"*10)
+        print("TIPO VIP:")
+        print(ladoC)
+        print("-"*10)
+        print(ladoD)
+        volver = input("Desea volver al MENU principal?(si/no)").lower().strip()
+        if volver == "si":
+            print("")
+            flag = True
         else:
-            if numAsiento > 0 and numAsiento <= 30:
-                print("Usted ha escogido el tipo de asiento NORMAL")
-                cantAsientoN = int(input("Indique cuantos asientos NORMALES reservara: "))
-                if cantAsientoN > 0:
-                    print("Usted ha reservado", cantAsientoN, "asientos NORMALES")
-
-    if opcionM == 2:
-        print("")
-
-    if opcionM == 3:
-        print("")
-
-    if opcionM == 4:
-        print("")
+          if volver == "no":
+              print("")
+              flag = False
 
 
+  if opcionM == 2:
+      print("Para realizar la compra de un boleto debe ingresar los siguientes datos: ")
+      nombrePasajero = input("Ingrese su nombre:\t").upper()
+      rutPasajero = input("Ingrese su rut:\t\t")
+      telefonoPasajero = int(input("Ingrese su N° celular:\t"))
+      bancoPasajero = input("Ingrese nombre de su banco:\t").upper()
 
+  if opcionM == 3:
+      print("")
 
+  if opcionM == 4:
+      print("")
+
+print("Hasta pronto")
